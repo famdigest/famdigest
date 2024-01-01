8bf44f70c5107c310c3ea6d041b1f0d15097e42c
Franco Valdes <franco.valdes89@gmail.com>
2024-01-01 14:24:08 -0500

    fix main site

--- apps/marketing/remix.config.js
+++ apps/marketing/remix.config.js
@@ -7,6 +7,22 @@ export default {
   tailwind: true,
   postcss: true,
   serverModuleFormat: "esm",
+  serverDependenciesToBundle: [
+    /endent/,
+    /dedent/,
+    /objectorarray/,
+    /fast-json-parse/,
+    /ical/,
+    /node-ical/,
+    /\@repo\/plugins/,
+    /\@repo\/database/,
+    /\@repo\/notifications/,
+    /drizzle-orm/,
+    /postgres/,
+    /\@react-email/,
+    /\@radix-ui/,
+    /\@babel/,
+  ],
   routes: async (defineRoutes) => {
     return flatRoutes("routes", defineRoutes, {
       ignoredRouteFiles: [
